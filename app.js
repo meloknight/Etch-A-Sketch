8133e2ac9b19a86d96eccf1084d67f0c023e557f
brody <49502879+meloknight@users.noreply.github.com>
2024-01-31 14:25:15 -0800

    cleaned up a bit of formatting.

--- app.js
+++ app.js
@@ -1,109 +1,97 @@
-const gridContainer = document.querySelector('.grid-container');
-const resizeButton = document.querySelector('#resize-btn');
-const resetButton = document.querySelector('#reset-btn');
+const gridContainer = document.querySelector(".grid-container");
+const resizeButton = document.querySelector("#resize-btn");
+const resetButton = document.querySelector("#reset-btn");
 
 let width = 10;
 
-for (let i = 0; i < width*width; i++) {
-    gridContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`
+for (let i = 0; i < width * width; i++) {
+  gridContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
 
-    const square = document.createElement('div');
-    square.classList.add('square');
-    
-    gridContainer.appendChild(square);
-};
+  const square = document.createElement("div");
+  square.classList.add("square");
 
-gridContainer.childNodes[0].remove()
+  gridContainer.appendChild(square);
+}
 
-gridContainer.childNodes.forEach(function(element) {
-    element.addEventListener('mouseover', (event) => {
-        
-        element.classList.add('shaded');
-    })
+gridContainer.childNodes[0].remove();
 
+gridContainer.childNodes.forEach(function (element) {
+  element.addEventListener("mouseover", (event) => {
+    element.classList.add("shaded");
+  });
 });
 
+resizeButton.addEventListener("click", function (e) {
+  let newGridWidthPrompt = prompt(
+    "What is the width of the new grid? (Enter a whole number between 1 and 100): "
+  );
 
-    // RESIZE THE BLOODY GRID!!
-resizeButton.addEventListener('click', function(e) { 
+  let newGridWidth = parseInt(newGridWidthPrompt);
 
-    let newGridWidthPrompt = prompt("What is the width of the new grid? (Enter a whole number between 1 and 100): ")
+  if (Number.isInteger(newGridWidth) === false) {
+    alert(
+      "Follow the instructions please :) I'm setting the grid width to 10 until you do :)"
+    );
+    newGridWidth = 10;
+  }
+  if (newGridWidth > 100) {
+    alert(
+      "Oops, number too high! I'll just go ahead and set grid width to 100."
+    );
+    newGridWidth = 100;
+  }
+  if (newGridWidth < 1) {
+    alert("Oops, number too low! I'll just go ahead and set grid width to 1.");
+    newGridWidth = 1;
+  }
 
-    let newGridWidth = parseInt(newGridWidthPrompt);
+  let gridToRemove = gridContainer.childNodes.length;
+  console.log(gridToRemove);
 
-    if (Number.isInteger(newGridWidth) === false) {
-        alert("Follow the instructions please :) I'm setting the grid width to 10 until you do :)")
-        newGridWidth = 10;
-    }
-    if (newGridWidth > 100) {
-        alert("Oops, number too high! I'll just go ahead and set grid width to 100.")
-        newGridWidth = 100;
-    }
-    if (newGridWidth < 1) {
-        alert("Oops, number too low! I'll just go ahead and set grid width to 1.")
-        newGridWidth = 1;
-    }
+  for (let i = 0; i < gridToRemove; i++) {
+    gridContainer.childNodes[0].remove();
+  }
 
-    let gridToRemove = gridContainer.childNodes.length;
-    console.log(gridToRemove);
+  for (let i = 0; i < newGridWidth * newGridWidth; i++) {
+    gridContainer.style.gridTemplateColumns = `repeat(${newGridWidth}, 1fr)`;
 
-    for (let i = 0; i < gridToRemove; i++) {
-        gridContainer.childNodes[0].remove();
-    }
-    
-    for (let i = 0; i < newGridWidth*newGridWidth; i++) {
-        gridContainer.style.gridTemplateColumns = `repeat(${newGridWidth}, 1fr)`
-    
-        const square = document.createElement('div');
-        square.classList.add('square');
-        
-        gridContainer.appendChild(square);
-    };
+    const square = document.createElement("div");
+    square.classList.add("square");
 
-    gridContainer.childNodes.forEach(function(element) {
-        element.addEventListener('mouseover', (event) => {
-            element.classList.add('shaded');
-        })
+    gridContainer.appendChild(square);
+  }
+
+  gridContainer.childNodes.forEach(function (element) {
+    element.addEventListener("mouseover", (event) => {
+      element.classList.add("shaded");
     });
-})
+  });
+});
 
+// RESET THE BLOODY GRID!!
+resetButton.addEventListener("click", function (e) {
+  let gridToRemove = gridContainer.childNodes.length;
 
-    // RESET THE BLOODY GRID!!
-    resetButton.addEventListener('click', function(e) { 
-    
-        let gridToRemove = gridContainer.childNodes.length;
-    
-        let resetGridSize = gridContainer.childNodes.length;
+  let resetGridSize = gridContainer.childNodes.length;
 
-        for (let i = 0; i < gridToRemove; i++) {
-            gridContainer.childNodes[0].remove();
-        }
-        
-        for (let i = 0; i < resetGridSize; i++) {
-            gridContainer.style.gridTemplateColumns = `repeat(${Math.sqrt(resetGridSize)}, 1fr)`
-        
-            const square = document.createElement('div');
-            square.classList.add('square');
-            
-            gridContainer.appendChild(square);
-        };
-    
-        gridContainer.childNodes.forEach(function(element) {
-            element.addEventListener('mouseover', (event) => {
-                element.classList.add('shaded');
-            })
-        });
+  for (let i = 0; i < gridToRemove; i++) {
+    gridContainer.childNodes[0].remove();
+  }
+
+  for (let i = 0; i < resetGridSize; i++) {
+    gridContainer.style.gridTemplateColumns = `repeat(${Math.sqrt(
+      resetGridSize
+    )}, 1fr)`;
+
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    gridContainer.appendChild(square);
+  }
+
+  gridContainer.childNodes.forEach(function (element) {
+    element.addEventListener("mouseover", (event) => {
+      element.classList.add("shaded");
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
+});
